6fe24b2114325ab5fb7e8dcf8485fc64cc038c00
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-05-26 18:34:30 +0000

    init

--- answer.php
+++ answer.php
@@ -3,18 +3,18 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Weather Today, with API & JSON" />
+  <meta name="description" content="Random Extinct Animal Facts, with JS (API)" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Ain Jeong" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.cyan-deep_orange.min.css" />
-  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
-  <link rel="manifest" href="./site.webmanifest" />
-  <title>Weather Today, with API & JSON</title>
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.brown-deep_orange.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Random Extinct Animal Facts, with JS (API)</title>
 </head>
 
 <body>
@@ -22,36 +22,41 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Weather Today, with API & JSON</span>
+        <span class="mdl-layout-title">Random Extinct Animal Facts, with JS (API)</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <br />
-      <?php
-      $apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5';
-      $dataFromApi = file_get_contents($apiUrl);
+      <div class="page-content-answer">
+        <?php
+        $apiUrl = 'https://extinct-api.herokuapp.com/api/v1/animal/';
+        $dataFromApi = file_get_contents($apiUrl);
 
-      if ($dataFromApi !== false) {
-        $jsonData = json_decode($dataFromApi, true);
+        if ($dataFromApi !== false) {
+          $jsonData = json_decode($dataFromApi, true);
 
-        // bring the information
-        $iconCode = $jsonData['weather'][0]['icon'];
-        $weatherMain = $jsonData['weather'][0]['main'];
-        $weatherDescription = $jsonData['weather'][0]['description'];
-        $tempKelvin = $jsonData['main']['temp'];
-        $tempCelsius = $tempKelvin - 273.15;
+          // bring the information from API
+          $imageSrc = $jsonData['data'][0]['imageSrc'];
+          $commonName = $jsonData['data'][0]['commonName'];
+          $binomialName = $jsonData['data'][0]['binomialName'];
+          $location = $jsonData['data'][0]['location'];
+          $lastRecord = $jsonData['data'][0]['lastRecord'];
+          $shortDescription = $jsonData['data'][0]['shortDesc'];
 
-        // output
-        echo '<img src="https://openweathermap.org/img/wn/' . $iconCode . '@2x.png" alt="weather icon">';
-        echo 'Weather: ' . $weatherMain . '; ' . $weatherDescription . '<br />';
-        echo 'Temperature: ' . number_format($tempCelsius, 1) . ' °C';
-      } else {
-        echo 'Error fetching weather data.';
-      }
-      ?>
-      <div class="page-content-return">
-        <a href="./index.php">Return ...</a>
+          // output
+          echo '<img src="' . $imageSrc . '" alt="extinct animal image">';
+          echo '<b>Name:</b> ' . $commonName . ' (' . $binomialName . ')<br />' .
+            '<b>Location:</b> ' . $location . '<br />' .
+            '<b>Last Record:</b> ' . $lastRecord . '<br />' .
+            '<b>Short Description:</b> ' . $shortDescription . '<br />';
+        } else {
+          echo 'Error fetching data.';
+        }
+        ?>
       </div>
+        <div class="page-content-return">
+          <a href="./index.php">Return ...</a>
+        </div>
     </main>
   </div>
 </body>
--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Weather Today, with API & JSON" />
+  <meta name="description" content="Random Extinct Animal Facts, with JS (API)" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Ain Jeong" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.cyan-deep_orange.min.css" />
-  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
-  <link rel="manifest" href="./site.webmanifest" />
-  <title>Weather Today, with API & JSON</title>
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.brown-deep_orange.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Random Extinct Animal Facts, with JS (API)</title>
 </head>
 
 <body>
@@ -22,23 +22,22 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Weather Today, with API & JSON</span>
+        <span class="mdl-layout-title">Random Extinct Animal Facts, with JS (API)</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="https://openweathermap.org/img/wn/03d@2x.png" alt="cloud image" width="95%" height="auto">
+        <img id="random-image" src="./images/question-mark.png" alt="Random image" width="95%" height="auto">
       </div>
       <br />
       <form action="answer.php" method="get">
         <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
           type="submit">
-          Weather Today
+          Check Random Facts
         </button>
       </form>
       <div class="page-content-answer">
-        <div id="weather-today-number"></div>
-        <div id="weather-today"></div>
+        <div id="extinct-animal-facts"></div>
       </div>
     </main>
   </div>
